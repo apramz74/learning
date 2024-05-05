b7d72e5a83f7d9dacbc9b154fc320f3d9c1bcb69
Adam Pramono <apramono@adams-mbp.mynetworksettings.com>
2024-05-05 15:22:55 -0400

    ma 2ndo commit

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 1, description: "Poopoorts", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Test", quantity: 33, packed: true },
 ];
